5186d56c7480b5427f185f69271ba3d0ee89e545
ALAOUI Yassine <yassine.alaoui@sully-group.fr>
2018-03-05 09:18:45 +0100

    Transferred the menu footer button into a component

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FootermenuComponent } from './footermenu/footermenu';
+@NgModule({
+	declarations: [FootermenuComponent],
+	imports: [],
+	exports: [FootermenuComponent]
+})
+export class ComponentsModule {}
---
+++ src/components/footermenu/footermenu.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import {ActionSheetController, AlertController} from "ionic-angular";
+import {SettingsProvider} from "../../providers/settings/settings";
+
+@Component({
+  selector: 'footermenu',
+  templateUrl: 'footermenu.html'
+})
+export class FootermenuComponent {
+
+  text: string;
+  selectedTheme: string;
+
+  constructor(private alertCtrl: AlertController, private settings: SettingsProvider, private actionSheetCtrl: ActionSheetController) {
+    this.settings.getActiveTheme().subscribe(val => this.selectedTheme = val);
+  }
+
+    theme(col: string){
+        this.settings.setActiveTheme(col);
+    }
+
+    showRadio() {
+        let alert = this.alertCtrl.create();
+        alert.setTitle('Set Background Color');
+
+        alert.addInput({
+            type: 'radio',
+            label: 'white',
+            value: 'light',
+        });
+
+        alert.addInput({
+            type: 'radio',
+            label: 'dark',
+            value: 'dark'
+        });
+
+        alert.addButton('Cancel');
+        alert.addButton({
+            text: 'OK',
+            handler: data => {
+                this.theme(data);
+            }
+        });
+        alert.present();
+    }
+
+    showActionSheet(){
+        let menuActionSheet = this.actionSheetCtrl.create({
+            title: 'Browse',
+            buttons: [
+                {
+                    text: 'Change theme',
+                    handler: () => {
+                        this.showRadio();
+                    }
+                },{
+                    text: 'Option 2',
+                    handler: () => {
+                        console.log('op2 clicked');
+                    }
+                },{
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Cancel clicked');
+                    }
+                }
+            ]
+        });
+        menuActionSheet.present();
+    }
+}
